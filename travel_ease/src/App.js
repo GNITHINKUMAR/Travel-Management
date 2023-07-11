04fd13ef4174a5ea0d261a780c317d0f9230404e
Guru1826 <imguru1826@gmail.com>
2023-07-11 13:12:05 +0530

    till navbar

---
+++ travel_ease/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './app.css'
+import Navbar from './Components/Navbar/Navbar'
+
+const App = () => {
+  return (
+
+    <div>
+    <Navbar/>
+
+  {/* <Home/>
+    <Main/>
+  <Footer/>  */}
+
+    </div>
+  
+  )
+}
+
+export default App
